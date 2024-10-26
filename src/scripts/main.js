8f6341cd8d846a09c54a26feed98dc3a38524a97
Luan Ferreira <luan.lnf94@gmail.com>
2024-10-26 10:46:16 -0300

    finalizando a pagina

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log("olá mundo");
+AOS.init();
+
+const dataDoEvento = new Date("Oct 31, 2024 20:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime(); 
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMS = 1000 * 60 * 60 ;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs / horaEmMS));
+    const minutosAteOEvento = Math.floor ((distanciaAteOEvento % horaEmMS) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs)/ 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if(distanciaAteOEvento < 0){
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
